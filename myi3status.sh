87f3ca43c2464a832456d8388eb5090ed5107e28
Stefan <stefan@layereight.de>
2018-04-29 22:07:08 +0200

    update unicode symbols

--- myi3status.sh
+++ myi3status.sh
@@ -7,7 +7,8 @@ i3status --config ${i3status_conf} | while :
 do
     read line
     LAYOUT=$(~/bin/xkb-switch)
-    BRIGHT=$(cat /sys/class/backlight/acpi_video0/brightness)
-    dat=", { \"full_text\": \"💡 $BRIGHT\", \"color\": \"#999900\"}, { \"full_text\": \"🖮 $LAYOUT\", \"color\":\"#FF8C00\" }]"
+    BRIGHT=$(cat /sys/class/backlight/intel_backlight/brightness)
+    dat=", { \"full_text\": \"💡$BRIGHT\", \"color\": \"#999900\"}, { \"full_text\": \"🅰$LAYOUT\", \"color\":\"#FF8C00\" }]"
     echo "${line/]/$dat}" || exit 1
 done
+
